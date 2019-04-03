d577ce191b0faf3f58869d4893085593a4d91b1e
HanLess <happydroll@163.com>
2019-04-03 11:49:40 +0800

    init

---
+++ react-master/packages/react-reconciler/src/ReactFiberReconciler.js
@@ -0,0 +1,473 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+import type {Fiber} from './ReactFiber';
+import type {FiberRoot} from './ReactFiberRoot';
+import type {
+  Instance,
+  TextInstance,
+  Container,
+  PublicInstance,
+} from './ReactFiberHostConfig';
+import type {ReactNodeList} from 'shared/ReactTypes';
+import type {ExpirationTime} from './ReactFiberExpirationTime';
+
+import {
+  findCurrentHostFiber,
+  findCurrentHostFiberWithNoPortals,
+} from 'react-reconciler/reflection';
+import {get as getInstance} from 'shared/ReactInstanceMap';
+import {HostComponent, ClassComponent} from 'shared/ReactWorkTags';
+import getComponentName from 'shared/getComponentName';
+import invariant from 'shared/invariant';
+import warningWithoutStack from 'shared/warningWithoutStack';
+import ReactSharedInternals from 'shared/ReactSharedInternals';
+
+import {getPublicInstance} from './ReactFiberHostConfig';
+import {
+  findCurrentUnmaskedContext,
+  processChildContext,
+  emptyContextObject,
+  isContextProvider as isLegacyContextProvider,
+} from './ReactFiberContext';
+import {createFiberRoot} from './ReactFiberRoot';
+import {injectInternals} from './ReactFiberDevToolsHook';
+import {
+  computeUniqueAsyncExpiration,
+  requestCurrentTime,
+  computeExpirationForFiber,
+  scheduleWork,
+  requestWork,
+  flushRoot,
+  batchedUpdates,
+  unbatchedUpdates,
+  flushSync,
+  flushControlled,
+  deferredUpdates,
+  syncUpdates,
+  interactiveUpdates,
+  flushInteractiveUpdates,
+  flushPassiveEffects,
+} from './ReactFiberScheduler';
+import {createUpdate, enqueueUpdate} from './ReactUpdateQueue';
+import ReactFiberInstrumentation from './ReactFiberInstrumentation';
+import {
+  getStackByFiberInDevAndProd,
+  phase as ReactCurrentFiberPhase,
+  current as ReactCurrentFiberCurrent,
+} from './ReactCurrentFiber';
+import {StrictMode} from './ReactTypeOfMode';
+import {Sync} from './ReactFiberExpirationTime';
+
+type OpaqueRoot = FiberRoot;
+
+// 0 is PROD, 1 is DEV.
+// Might add PROFILE later.
+type BundleType = 0 | 1;
+
+type DevToolsConfig = {|
+  bundleType: BundleType,
+  version: string,
+  rendererPackageName: string,
+  // Note: this actually *does* depend on Fiber internal fields.
+  // Used by "inspect clicked DOM element" in React DevTools.
+  findFiberByHostInstance?: (instance: Instance | TextInstance) => Fiber,
+  // Used by RN in-app inspector.
+  // This API is unfortunately RN-specific.
+  // TODO: Change it to accept Fiber instead and type it properly.
+  getInspectorDataForViewTag?: (tag: number) => Object,
+|};
+
+let didWarnAboutNestedUpdates;
+let didWarnAboutFindNodeInStrictMode;
+
+if (__DEV__) {
+  didWarnAboutNestedUpdates = false;
+  didWarnAboutFindNodeInStrictMode = {};
+}
+
+function getContextForSubtree(
+  parentComponent: ?React$Component<any, any>,
+): Object {
+  if (!parentComponent) {
+    return emptyContextObject;
+  }
+
+  const fiber = getInstance(parentComponent);
+  const parentContext = findCurrentUnmaskedContext(fiber);
+
+  if (fiber.tag === ClassComponent) {
+    const Component = fiber.type;
+    if (isLegacyContextProvider(Component)) {
+      return processChildContext(fiber, Component, parentContext);
+    }
+  }
+
+  return parentContext;
+}
+/**
+ *  节点的更新
+ * 
+ * 把多个 update 存在 updateQueue 里，在后续统一处理更新，执行 scheduleWork 通知 react 进行调度，根据任务的优先级进行更新。
+ * vue 是直接执行 patch 方法，对比新旧 vnode
+ * 
+ * 执行时机：
+ * vue 是在微事件中处理（异步）
+ * React 是计算出一个节点的更新时间
+ */
+
+
+function scheduleRootUpdate(
+  current: Fiber,
+  element: ReactNodeList,
+  expirationTime: ExpirationTime,
+  callback: ?Function,
+) {
+  if (__DEV__) {
+    if (
+      ReactCurrentFiberPhase === 'render' &&
+      ReactCurrentFiberCurrent !== null &&
+      !didWarnAboutNestedUpdates
+    ) {
+      didWarnAboutNestedUpdates = true;
+      warningWithoutStack(
+        false,
+        'Render methods should be a pure function of props and state; ' +
+          'triggering nested component updates from render is not allowed. ' +
+          'If necessary, trigger nested updates in componentDidUpdate.\n\n' +
+          'Check the render method of %s.',
+        getComponentName(ReactCurrentFiberCurrent.type) || 'Unknown',
+      );
+    }
+  }
+
+  const update = createUpdate(expirationTime);
+  // Caution: React DevTools currently depends on this property
+  // being called "element".
+  update.payload = {element};
+
+  callback = callback === undefined ? null : callback;
+  if (callback !== null) {
+    warningWithoutStack(
+      typeof callback === 'function',
+      'render(...): Expected the last optional `callback` argument to be a ' +
+        'function. Instead received: %s.',
+      callback,
+    );
+    update.callback = callback;
+  }
+
+  flushPassiveEffects();
+  /**
+   * 这里根 Vue 思路是一样的
+   * 从根节点开始更新，递归执行更新子节点
+   * 
+   * 差异：
+   * （1）vue是放在异步中执行更新，react是算出一个更新时间
+   * （2）vue是通过观察者模式，每次更新数据都会执行更新；
+   *     react 需要手动 setData，当短时间内多次 setData，每次都执行更新显然不合适，所以使用了 updateQueue 来收集 update 的内容，在合适的时机统一更新
+   */
+  // enqueueUpdate 是收集更新的过程
+  enqueueUpdate(current, update);
+  // scheduleWork 是执行更新的过程
+  scheduleWork(current, expirationTime);
+
+  return expirationTime;
+}
+
+export function updateContainerAtExpirationTime(
+  element: ReactNodeList,
+  container: OpaqueRoot,
+  parentComponent: ?React$Component<any, any>,
+  expirationTime: ExpirationTime,
+  callback: ?Function,
+) {
+  // TODO: If this is a nested container, this won't be the root.
+  const current = container.current;
+
+  if (__DEV__) {
+    if (ReactFiberInstrumentation.debugTool) {
+      if (current.alternate === null) {
+        ReactFiberInstrumentation.debugTool.onMountContainer(container);
+      } else if (element === null) {
+        ReactFiberInstrumentation.debugTool.onUnmountContainer(container);
+      } else {
+        ReactFiberInstrumentation.debugTool.onUpdateContainer(container);
+      }
+    }
+  }
+
+  const context = getContextForSubtree(parentComponent);
+  if (container.context === null) {
+    container.context = context;
+  } else {
+    container.pendingContext = context;
+  }
+
+  return scheduleRootUpdate(current, element, expirationTime, callback);
+}
+
+function findHostInstance(component: Object): PublicInstance | null {
+  const fiber = getInstance(component);
+  if (fiber === undefined) {
+    if (typeof component.render === 'function') {
+      invariant(false, 'Unable to find node on an unmounted component.');
+    } else {
+      invariant(
+        false,
+        'Argument appears to not be a ReactComponent. Keys: %s',
+        Object.keys(component),
+      );
+    }
+  }
+  const hostFiber = findCurrentHostFiber(fiber);
+  if (hostFiber === null) {
+    return null;
+  }
+  return hostFiber.stateNode;
+}
+
+function findHostInstanceWithWarning(
+  component: Object,
+  methodName: string,
+): PublicInstance | null {
+  if (__DEV__) {
+    const fiber = getInstance(component);
+    if (fiber === undefined) {
+      if (typeof component.render === 'function') {
+        invariant(false, 'Unable to find node on an unmounted component.');
+      } else {
+        invariant(
+          false,
+          'Argument appears to not be a ReactComponent. Keys: %s',
+          Object.keys(component),
+        );
+      }
+    }
+    const hostFiber = findCurrentHostFiber(fiber);
+    if (hostFiber === null) {
+      return null;
+    }
+    if (hostFiber.mode & StrictMode) {
+      const componentName = getComponentName(fiber.type) || 'Component';
+      if (!didWarnAboutFindNodeInStrictMode[componentName]) {
+        didWarnAboutFindNodeInStrictMode[componentName] = true;
+        if (fiber.mode & StrictMode) {
+          warningWithoutStack(
+            false,
+            '%s is deprecated in StrictMode. ' +
+              '%s was passed an instance of %s which is inside StrictMode. ' +
+              'Instead, add a ref directly to the element you want to reference.' +
+              '\n%s' +
+              '\n\nLearn more about using refs safely here:' +
+              '\nhttps://fb.me/react-strict-mode-find-node',
+            methodName,
+            methodName,
+            componentName,
+            getStackByFiberInDevAndProd(hostFiber),
+          );
+        } else {
+          warningWithoutStack(
+            false,
+            '%s is deprecated in StrictMode. ' +
+              '%s was passed an instance of %s which renders StrictMode children. ' +
+              'Instead, add a ref directly to the element you want to reference.' +
+              '\n%s' +
+              '\n\nLearn more about using refs safely here:' +
+              '\nhttps://fb.me/react-strict-mode-find-node',
+            methodName,
+            methodName,
+            componentName,
+            getStackByFiberInDevAndProd(hostFiber),
+          );
+        }
+      }
+    }
+    return hostFiber.stateNode;
+  }
+  return findHostInstance(component);
+}
+
+export function createContainer(
+  containerInfo: Container,
+  isConcurrent: boolean,
+  hydrate: boolean,
+): OpaqueRoot {
+  return createFiberRoot(containerInfo, isConcurrent, hydrate);
+}
+
+/** 
+ * 这个方法是 ReactDOM.render 的主要逻辑
+ * 
+ * currentTime 是一个时间差：
+ * requestCurrentTime 方法返回当前渲染时间直到 js 加载初的时间差值，差值小的话会返回一个固定的常量
+ * 
+ * computeExpirationForFiber 方法会根据当前渲染的 currentTime 计算出一个优先级时间。
+ * （1）互动式：优先级高，比如事件出发的，需要即使响应
+ * （2）异步的：低优先级，普通的异步事件
+ * （3）以 25 为单位增加，即 25ms 以内所有的操作算一个优先级，统一处理
+ * 
+*/
+export function updateContainer(
+  element: ReactNodeList,
+  container: OpaqueRoot,
+  parentComponent: ?React$Component<any, any>,
+  callback: ?Function,
+): ExpirationTime {
+  const current = container.current;
+  const currentTime = requestCurrentTime();
+  const expirationTime = computeExpirationForFiber(currentTime, current);
+  return updateContainerAtExpirationTime(
+    element,
+    container,
+    parentComponent,
+    expirationTime,
+    callback,
+  );
+}
+
+export {
+  flushRoot,
+  requestWork,
+  computeUniqueAsyncExpiration,
+  batchedUpdates,
+  unbatchedUpdates,
+  deferredUpdates,
+  syncUpdates,
+  interactiveUpdates,
+  flushInteractiveUpdates,
+  flushControlled,
+  flushSync,
+};
+
+export function getPublicRootInstance(
+  container: OpaqueRoot,
+): React$Component<any, any> | PublicInstance | null {
+  const containerFiber = container.current;
+  if (!containerFiber.child) {
+    return null;
+  }
+  switch (containerFiber.child.tag) {
+    case HostComponent:
+      return getPublicInstance(containerFiber.child.stateNode);
+    default:
+      return containerFiber.child.stateNode;
+  }
+}
+
+export {findHostInstance};
+
+export {findHostInstanceWithWarning};
+
+export function findHostInstanceWithNoPortals(
+  fiber: Fiber,
+): PublicInstance | null {
+  const hostFiber = findCurrentHostFiberWithNoPortals(fiber);
+  if (hostFiber === null) {
+    return null;
+  }
+  return hostFiber.stateNode;
+}
+
+let overrideHookState = null;
+let overrideProps = null;
+
+if (__DEV__) {
+  const copyWithSetImpl = (
+    obj: Object | Array<any>,
+    path: Array<string | number>,
+    idx: number,
+    value: any,
+  ) => {
+    if (idx >= path.length) {
+      return value;
+    }
+    const key = path[idx];
+    const updated = Array.isArray(obj) ? obj.slice() : {...obj};
+    // $FlowFixMe number or string is fine here
+    updated[key] = copyWithSetImpl(obj[key], path, idx + 1, value);
+    return updated;
+  };
+
+  const copyWithSet = (
+    obj: Object | Array<any>,
+    path: Array<string | number>,
+    value: any,
+  ): Object | Array<any> => {
+    return copyWithSetImpl(obj, path, 0, value);
+  };
+
+  // Support DevTools editable values for useState and useReducer.
+  overrideHookState = (
+    fiber: Fiber,
+    id: number,
+    path: Array<string | number>,
+    value: any,
+  ) => {
+    // For now, the "id" of stateful hooks is just the stateful hook index.
+    // This may change in the future with e.g. nested hooks.
+    let currentHook = fiber.memoizedState;
+    while (currentHook !== null && id > 0) {
+      currentHook = currentHook.next;
+      id--;
+    }
+    if (currentHook !== null) {
+      flushPassiveEffects();
+
+      const newState = copyWithSet(currentHook.memoizedState, path, value);
+      currentHook.memoizedState = newState;
+      currentHook.baseState = newState;
+
+      // We aren't actually adding an update to the queue,
+      // because there is no update we can add for useReducer hooks that won't trigger an error.
+      // (There's no appropriate action type for DevTools overrides.)
+      // As a result though, React will see the scheduled update as a noop and bailout.
+      // Shallow cloning props works as a workaround for now to bypass the bailout check.
+      fiber.memoizedProps = {...fiber.memoizedProps};
+
+      scheduleWork(fiber, Sync);
+    }
+  };
+
+  // Support DevTools props for function components, forwardRef, memo, host components, etc.
+  overrideProps = (fiber: Fiber, path: Array<string | number>, value: any) => {
+    flushPassiveEffects();
+    fiber.pendingProps = copyWithSet(fiber.memoizedProps, path, value);
+    if (fiber.alternate) {
+      fiber.alternate.pendingProps = fiber.pendingProps;
+    }
+    scheduleWork(fiber, Sync);
+  };
+}
+
+export function injectIntoDevTools(devToolsConfig: DevToolsConfig): boolean {
+  const {findFiberByHostInstance} = devToolsConfig;
+  const {ReactCurrentDispatcher} = ReactSharedInternals;
+
+  return injectInternals({
+    ...devToolsConfig,
+    overrideHookState,
+    overrideProps,
+    currentDispatcherRef: ReactCurrentDispatcher,
+    findHostInstanceByFiber(fiber: Fiber): Instance | TextInstance | null {
+      const hostFiber = findCurrentHostFiber(fiber);
+      if (hostFiber === null) {
+        return null;
+      }
+      return hostFiber.stateNode;
+    },
+    findFiberByHostInstance(instance: Instance | TextInstance): Fiber | null {
+      if (!findFiberByHostInstance) {
+        // Might not be implemented by the renderer.
+        return null;
+      }
+      return findFiberByHostInstance(instance);
+    },
+  });
+}
---
+++ react-master/packages/react-reconciler/src/ReactFiberRoot.js
@@ -0,0 +1,185 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+import type {Fiber} from './ReactFiber';
+import type {ExpirationTime} from './ReactFiberExpirationTime';
+import type {TimeoutHandle, NoTimeout} from './ReactFiberHostConfig';
+import type {Thenable} from './ReactFiberScheduler';
+import type {Interaction} from 'scheduler/src/Tracing';
+
+import {noTimeout} from './ReactFiberHostConfig';
+import {createHostRootFiber} from './ReactFiber';
+import {NoWork} from './ReactFiberExpirationTime';
+import {enableSchedulerTracing} from 'shared/ReactFeatureFlags';
+import {unstable_getThreadID} from 'scheduler/tracing';
+
+// TODO: This should be lifted into the renderer.
+export type Batch = {
+  _defer: boolean,
+  _expirationTime: ExpirationTime,
+  _onComplete: () => mixed,
+  _next: Batch | null,
+};
+
+export type PendingInteractionMap = Map<ExpirationTime, Set<Interaction>>;
+
+type BaseFiberRootProperties = {|
+  // Any additional information from the host associated with this root.
+  containerInfo: any,
+  // Used only by persistent updates.
+  pendingChildren: any,
+  // The currently active root fiber. This is the mutable root of the tree.
+  current: Fiber,
+
+  // The following priority levels are used to distinguish between 1)
+  // uncommitted work, 2) uncommitted work that is suspended, and 3) uncommitted
+  // work that may be unsuspended. We choose not to track each individual
+  // pending level, trading granularity for performance.
+  //
+  // The earliest and latest priority levels that are suspended from committing.
+  earliestSuspendedTime: ExpirationTime,
+  latestSuspendedTime: ExpirationTime,
+  // The earliest and latest priority levels that are not known to be suspended.
+  earliestPendingTime: ExpirationTime,
+  latestPendingTime: ExpirationTime,
+  // The latest priority level that was pinged by a resolved promise and can
+  // be retried.
+  latestPingedTime: ExpirationTime,
+
+  pingCache:
+    | WeakMap<Thenable, Set<ExpirationTime>>
+    | Map<Thenable, Set<ExpirationTime>>
+    | null,
+
+  // If an error is thrown, and there are no more updates in the queue, we try
+  // rendering from the root one more time, synchronously, before handling
+  // the error.
+  didError: boolean,
+
+  pendingCommitExpirationTime: ExpirationTime,
+  // A finished work-in-progress HostRoot that's ready to be committed.
+  finishedWork: Fiber | null,
+  // Timeout handle returned by setTimeout. Used to cancel a pending timeout, if
+  // it's superseded by a new one.
+  timeoutHandle: TimeoutHandle | NoTimeout,
+  // Top context object, used by renderSubtreeIntoContainer
+  context: Object | null,
+  pendingContext: Object | null,
+  // Determines if we should attempt to hydrate on the initial mount
+  +hydrate: boolean,
+  // Remaining expiration time on this root.
+  // TODO: Lift this into the renderer
+  nextExpirationTimeToWorkOn: ExpirationTime,
+  expirationTime: ExpirationTime,
+  // List of top-level batches. This list indicates whether a commit should be
+  // deferred. Also contains completion callbacks.
+  // TODO: Lift this into the renderer
+  firstBatch: Batch | null,
+  // Linked-list of roots
+  nextScheduledRoot: FiberRoot | null,
+|};
+
+// The following attributes are only used by interaction tracing builds.
+// They enable interactions to be associated with their async work,
+// And expose interaction metadata to the React DevTools Profiler plugin.
+// Note that these attributes are only defined when the enableSchedulerTracing flag is enabled.
+type ProfilingOnlyFiberRootProperties = {|
+  interactionThreadID: number,
+  memoizedInteractions: Set<Interaction>,
+  pendingInteractionMap: PendingInteractionMap,
+|};
+
+// Exported FiberRoot type includes all properties,
+// To avoid requiring potentially error-prone :any casts throughout the project.
+// Profiling properties are only safe to access in profiling builds (when enableSchedulerTracing is true).
+// The types are defined separately within this file to ensure they stay in sync.
+// (We don't have to use an inline :any cast when enableSchedulerTracing is disabled.)
+export type FiberRoot = {
+  ...BaseFiberRootProperties,
+  ...ProfilingOnlyFiberRootProperties,
+};
+
+export function createFiberRoot(
+  containerInfo: any,
+  isConcurrent: boolean,
+  hydrate: boolean,
+): FiberRoot {
+  // Cyclic construction. This cheats the type system right now because
+  // stateNode is any.
+  const uninitializedFiber = createHostRootFiber(isConcurrent);
+
+  let root;
+  if (enableSchedulerTracing) {
+    root = ({
+      current: uninitializedFiber, // Fiber 对象通过 new Fiber 创建，与 ReactElement 对应也是树状结构，这里树的顶点
+      containerInfo: containerInfo, // 通过 render 方法传入的 root 节点, 应用挂载的节点
+      pendingChildren: null,  // ssr 用来复用节点的
+      // 任务调度的时间标记优先级
+      earliestPendingTime: NoWork,
+      latestPendingTime: NoWork,
+      earliestSuspendedTime: NoWork,
+      latestSuspendedTime: NoWork,
+      latestPingedTime: NoWork,
+      // 标记渲染过程中是否有错误
+      pingCache: null,
+      // 正在等待提交的任务
+      didError: false,
+
+      pendingCommitExpirationTime: NoWork,
+      finishedWork: null, // 记录一次更新渲染过程完成的任务 Fiber 对象
+      timeoutHandle: noTimeout,
+      context: null,
+      pendingContext: null,
+      hydrate,
+      nextExpirationTimeToWorkOn: NoWork, // 标记此次更新要执行的是哪个优先级的任务
+      expirationTime: NoWork, // 用在调度中
+      firstBatch: null,
+      nextScheduledRoot: null,  // 链表属性
+
+      interactionThreadID: unstable_getThreadID(),
+      memoizedInteractions: new Set(),
+      pendingInteractionMap: new Map(),
+    }: FiberRoot);
+  } else {
+    root = ({
+      current: uninitializedFiber,
+      containerInfo: containerInfo,
+      pendingChildren: null,
+
+      pingCache: null,
+
+      earliestPendingTime: NoWork,
+      latestPendingTime: NoWork,
+      earliestSuspendedTime: NoWork,
+      latestSuspendedTime: NoWork,
+      latestPingedTime: NoWork,
+
+      didError: false,
+
+      pendingCommitExpirationTime: NoWork,
+      finishedWork: null,
+      timeoutHandle: noTimeout,
+      context: null,
+      pendingContext: null,
+      hydrate,
+      nextExpirationTimeToWorkOn: NoWork,
+      expirationTime: NoWork,
+      firstBatch: null,
+      nextScheduledRoot: null,
+    }: BaseFiberRootProperties);
+  }
+
+  uninitializedFiber.stateNode = root;
+
+  // The reason for the way the Flow types are structured in this file,
+  // Is to avoid needing :any casts everywhere interaction tracing fields are used.
+  // Unfortunately that requires an :any cast for non-interaction tracing capable builds.
+  // $FlowFixMe Remove this :any cast and replace it with something better.
+  return ((root: any): FiberRoot);
+}
